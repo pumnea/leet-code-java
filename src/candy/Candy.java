21ed213f6250647d06076a4f3f1c15cfd7255842
pumnea <apumnea.dev@gmail.com>
2024-11-07 00:24:20 +0200

    Solution for Candy problem

---
+++ src/candy/Candy.java
@@ -0,0 +1,48 @@
+package candy;
+
+/**
+ * There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+ * You are giving candies to these children subjected to the following requirements:
+ * Each child must have at least one candy.
+ * Children with a higher rating get more candies than their neighbors.
+ * Return the minimum number of candies you need to have to distribute the candies to the children.
+ *
+ * @author Alex Pumnea
+ */
+public class Candy {
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+
+        int len = ratings.length;
+        int candies = 1;
+        int up = 1;
+        int down = 0;
+        int peak = 1;
+
+        for (int i = 1; i < len; i++) {
+
+            int current = ratings[i];
+            int previous = ratings[i - 1];
+
+            if (current > previous) {
+                up++;
+                peak = up;
+                down = 0;
+                candies += up;
+            } else if (current < previous) {
+                down++;
+                up = 1;
+                candies += down;
+                if (down >= peak) {
+                    candies++;
+                }
+            } else {
+                up = 1;
+                peak = 1;
+                down = 0;
+                candies++;
+            }
+        }
+        return candies;
+    }
+}
